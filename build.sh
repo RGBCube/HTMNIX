76c9a74f8beb8bf18dc6bf68f46a353ba2b74325
RGBCube <git@rgbcu.be>
2024-02-28 12:52:27 +0300

    Fix _ignored file checking

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ for dir in $(find site -type d); do
 done
 
 for file in $(find site -type f); do
-  if [[ ! "$file" =~ ^_ ]]; then
+  if [[ ! "${file##*/}" =~ ^_ ]]; then
     if [[ "$file" =~ .nix$ ]]; then
       echo "Processing file $file to _${file%.nix}.html..."
       TARGET_FILE=$(realpath "$file") nix eval "${HTMNIX_REF:-github:RGBCube/HTMNIX}#result" --impure --raw --apply toString > "_${file%.nix}.html"
